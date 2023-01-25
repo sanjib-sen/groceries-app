a3e32d3999b3f5c10c0d67d19aab7dcf2782d2ce
Sanjib Kumar Sen <sksenonline@gmail.com>
2023-01-26 02:49:52 +0600

    currently only works for sessionStorage now

--- src/app/middleware.tsx
+++ src/app/middleware.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
   const [value, setValue] = useState<T>(() => {
-    const jsonValue = localStorage.getItem(key);
+    const jsonValue = sessionStorage.getItem(key);
     if (jsonValue == null) {
       if (typeof initialValue === "function") {
         return (initialValue as () => T)();
@@ -17,7 +17,7 @@ export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
   });
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
+    sessionStorage.setItem(key, JSON.stringify(value));
   }, [value, key]);
 
   return [value, setValue] as [T, typeof setValue];
